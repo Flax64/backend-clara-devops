29e222c67f07e4185c7623470fe7eac2c5cc17a1
Flavio Aguilar <flavio.red0568@gmail.com>
2026-05-04 17:28:28 -0600

    Feat: ajuste de puerto para contenedor http

--- backend_CLARA/Program.cs
+++ backend_CLARA/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.WebHost.UseUrls("http://*:8080");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
